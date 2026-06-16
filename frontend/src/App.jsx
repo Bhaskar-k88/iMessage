06d977b63be419be9e436748b0fdb26d06720460
Bhaskar-k88 <bhaskarbhaskark32@gmail.com>
2026-06-16 12:40:29 +0530

    deloying part

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+
+function App() {
+
+  return (
+    <>
+    <h1>iMessage application</h1>
+
+          <header>
+        <Show when="signed-out">
+          <SignInButton mode='modal' />
+          <SignUpButton mode='modal' />
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+
+    </>
+  )
+}
+
+export default App
